d9eb5e1af3428a70b6e0dd80524f19aea201c68b
kazzsss <satokazkei@yahoo.co.jp>
2026-07-27 10:34:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let candles = [];
    レート取得（USDJPY）
 ============================ */
 async function fetchRate() {
-  const url = "https://api.exchangerate-api.com/v4/latest/USD";
+const url = "https://open.er-api.com/v6/latest/USD";
   const res = await fetch(url);
   const data = await res.json();
   return data.rates.JPY;
